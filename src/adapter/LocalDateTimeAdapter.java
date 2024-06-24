c726adfa4871572b7d865abc4e84027c14b07bab
Rossana <rossanatotolo@yandex.ru>
2024-06-24 16:17:36 +0300

    Вторая версия ТЗ9

--- src/adapter/LocalDateTimeAdapter.java
+++ src/adapter/LocalDateTimeAdapter.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
-    private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
